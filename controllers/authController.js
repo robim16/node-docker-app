21cb95ef31f03ca2fe859e3169f142e6e5ce254a
robim16 <carteagajimenez@gmail.com>
2025-05-30 17:39:41 -0500

    creacion del user ctrl

---
+++ controllers/authController.js
@@ -0,0 +1,13 @@
+const User = require("../models/userModel")
+
+
+exports.signUp = async (req, res) => {
+    try {
+        const newUser = await User.create(req.body)
+        res.status(201).json({
+            
+        })
+    } catch (error) {
+        
+    }
+}
--- index.js
+++ index.js
@@ -2,23 +2,37 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
+const postRouter = require("./routes/postRoutes")
+
 const app = express();
 
 const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
 
-mongoose
-    .connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    })
-    .then(() => console.log("successfully connected to DB"))
-    .catch((err) => console.log(err))
+const connectWithRetry = () => {
 
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+        .then(() => console.log("successfully connected to DB"))
+        .catch((e) => {
+            console.log(e);
+            setTimeout(connectWithRetry, 5000)
+        })
+}
+
+
+connectWithRetry()
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Docker!!')
 })
+
+app.use("/api/v1/posts", postRouter)
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`app listen on port ${port}`))
